f9bd15da49dfeae15f1e517c68231605c18a34f8
Ezra Berendsen <berendsen@d-tt.nl>
2020-07-15 23:24:01 +0200

    Removed unnecessary new Main().

--- src/ezrab/nl/Main.java
+++ src/ezrab/nl/Main.java
@@ -5,10 +5,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        new Main();
-    }
-
-    private Main() {
         Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
 
